3581faeebd783f8984c8c5febf8ac3eba60cdb5e
rahathossain3 <rahathossain210@gmail.com>
2022-04-24 00:15:48 +0600

    get own api data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/users')
+      .then(res => res.json())
+      .then(data => setUsers(data));
+  }, [])
+
+  // handle user
+  const handleAddUser = event => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+
+    const user = { name, email }
+
+    //sent / post data to server
+
+
+
+
+
+
+
+
+
+
+    console.log(name, email);
+  }
+
+
+
   return (
     <div className="App">
-      <h1>My Own Data</h1>
+      <h1>My Own Data : {users.length} </h1>
+
+      <form onSubmit={handleAddUser}>
+        <input type="text" name='name' placeholder='Name' required />
+        <input type="text" name='email' placeholder='Email' required />
+        <input type="submit" value='Add user' />
+      </form>
+
+      <ul>
+        {
+          users.map(user => <li key={user.id}> id: {user.id}, Name: {user.name}, email: {user.email}</li>)
+        }
+      </ul>
+
     </div>
   );
 }
